f6141bc03551aad29446ce057dabe34c6f35c86a
Chaitanya Sharma <gopi.daiict@gmail.com>
2013-05-30 00:07:09 +0530

    updated helpers, added reload-dotfiles; nuked update.sh; update_check.sh

--- tools/helpers.sh
+++ tools/helpers.sh
@@ -1,3 +1,31 @@
 #!/bin/sh
 
 # helper function here!
+
+
+# reload all.
+function reload_dotfiles {
+    echo "\033[32m Reloading Dotfiles ..."
+    source $DOTFILES/.csharmarc
+}
+
+
+# update dotfiles.
+function update_dotfiles {
+    echo "\033[32m Updating Dotfiles ..."
+    
+    # stash away old dir.
+    OLD_DIR=`pwd`
+    
+    cd $DOTFILES
+
+    if git pull origin master
+    then
+	echo "Yeppie! Dotfiles Updated!"
+    else 
+	echo "No Updates!"
+    fi
+
+    # restore back to user dir
+    cd $OLD_DIR
+}
--- tools/update_check.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo "checking for updates!"
